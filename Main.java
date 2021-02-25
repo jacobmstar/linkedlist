622fcf2b6d29de346ff064ddd8d20357445e6248
jacobmstar <79279842+jacobmstar@users.noreply.github.com>
2021-02-25 14:38:51 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+
+  public static void main(String[] args) {
+    /*
+    theChain is 3, 4, 7, 6, 3, 2, 9, 6, 3, 6
+    */
+    LinkedList list = new LinkedList();
+    list.add(3);
+    list.add(4);
+    list.add(7);
+    list.add(6);
+    list.add(3);
+    list.add(2);
+    list.add(9);
+    list.add(6);
+    list.add(3);
+    list.add(6);
+
+    System.out.println(list);
+  }
+}
